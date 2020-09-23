3c636e54e6d20cc4020e58cde7cb132ad55663eb
= <=>
2020-09-23 16:55:58 -0300

    changed from json to properties

--- src/main/java/com/github/raonigabriel/VersionCommand.java
+++ src/main/java/com/github/raonigabriel/VersionCommand.java
@@ -1,10 +1,8 @@
 package com.github.raonigabriel;
 
 import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.Callable;
-
-import com.grack.nanojson.JsonObject;
-import com.grack.nanojson.JsonParser;
 
 import picocli.CommandLine.Command;
 
@@ -16,15 +14,16 @@ public class VersionCommand implements Callable<Integer> {
 
 		String version = "dev major.minor.revision";
 		try (InputStream is = getClass().getClassLoader().getResourceAsStream("git.properties")) {
-			JsonObject obj = JsonParser.object().from(is);
-			version = obj.getString("git.build.version")
-				+ " build " + obj.getString("git.commit.id.abbrev");
+			Properties props = new Properties();
+			props.load(is);
+			version = props.getProperty("git.build.version")
+				+ " build " + props.getProperty("git.commit.id.abbrev");
 		}
 		
 		System.out.printf(CheckSumApp.APP_NAME + " %s\n", version);
 		System.out.println("Written by Raoni Gabriel");
 		System.out.println();
-		System.out.println("Copyright (C) 2019 Raoni Gabriel.");
+		System.out.println("Copyright (C) 2020 Raoni Gabriel.");
 		System.out.println("This is free software; see the source for copying conditions.  There is NO");
 		System.out.println("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.");
 		return 0;
